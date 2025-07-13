7f7153527a2fc2789bb846b977eacd2e4e41c5eb
Om Veerwal <omveerwal9@gmail.com>
2025-07-13 08:53:42 +0530

    fetching ranodm user from api and showing it data

---
+++ random-user-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import RandomUser from './component/RandomUser'
+
+const App = () => {
+  return (
+    <div>
+      <RandomUser />
+    </div>
+  )
+}
+
+export default App
---
+++ random-user-app/src/component/RandomUser.jsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from 'react'
+
+const RandomUser = () => {
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    const fetchUser = async () => {
+        try {
+            setLoading(true);
+            const res = await fetch('https://randomuser.me/api/');
+            const data = await res.json();
+            console.log(data, 'api data');
+            setUsers([data.results[0]]);
+
+        } catch (err) {
+            console.log('Error while fetching data', err);
+        }
+        finally {
+            setLoading(false);
+        }
+    };
+    useEffect(() => {
+        fetchUser();
+    }, []);
+
+    return (
+        <div className='container'>
+            <h2 className='text-center'>Random User Data</h2>
+            {loading ? (
+                <div className="text-center">
+                    <div className="spinner-border text-primary" role="status">
+                        <span className="visually-hidden">Loading...</span>
+                    </div>
+                    </div>
+
+                    ) : (
+                    <div>
+                        {users.map((user, id) => (
+                            <div className='card-title' key={id}>
+                                <img src={user.picture.large} alt='user' />
+                                <h3 className='fw-bold'>
+                                    {user.name.title} {user.name.first} {user.name.last}
+
+                                </h3>
+                                <div className='card-body'>
+                                    <p className='fw-bold'>  {user.email} </p>
+
+                                </div>
+
+                            </div>
+
+                        ))}
+
+                    </div>
+            )}
+
+                    <button className='btn btn-primary ' onClick={fetchUser}>Fetch User</button>
+
+                </div>
+            )
+}
+
+            export default RandomUser
